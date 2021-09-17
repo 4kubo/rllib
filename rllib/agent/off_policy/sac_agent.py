d80035920e36a4abe1bac401fc535bb0db6407b8
4kubo <kubo-a@sys.i.kyoto-u.ac.jp>
2021-09-17 15:06:13 +0900

    [Mod] Performance improvement of SAC agent.
    - The training return in HalfCheetah-v3 changed from about 3000 to 5000
     at timesteps==100000

--- rllib/agent/off_policy/sac_agent.py
+++ rllib/agent/off_policy/sac_agent.py
@@ -1,13 +1,14 @@
 """Implementation of DQNAgent Algorithms."""
-from itertools import chain
 
 import torch.nn.modules.loss as loss
 from torch.optim import Adam
 
 from rllib.algorithms.sac import SAC
+from rllib.dataset.datatypes import Loss
 from rllib.policy import NNPolicy
+from rllib.util.neural_networks import DisableGradient
+from rllib.util.parameter_decay import Constant, Learnable
 from rllib.value_function import NNEnsembleQFunction
-
 from .off_policy_agent import OffPolicyAgent
 
 
@@ -33,6 +34,9 @@ class SACAgent(OffPolicyAgent):
         self,
         critic,
         policy,
+        actor_optimizer=None,
+        critic_optimizer=None,
+        temp_optimizer=None,
         criterion=loss.MSELoss,
         eta=0.2,
         entropy_regularization=False,
@@ -54,31 +58,98 @@ class SACAgent(OffPolicyAgent):
             **kwargs,
         )
 
-        self.optimizer = type(self.optimizer)(
-            [
-                p
-                for n, p in self.algorithm.named_parameters()
-                if "target" not in n and "old_policy" not in n
-            ],
-            **self.optimizer.defaults,
-        )
+        self.actor_optimizer = actor_optimizer
+        self.critic_optimizer = critic_optimizer
+        self.temp_optimizer = temp_optimizer
         self.policy = self.algorithm.policy
 
-    @classmethod
-    def default(cls, environment, policy=None, critic=None, lr=3e-4, *args, **kwargs):
-        """See `AbstractAgent.default'."""
-        if critic is None:
-            critic = NNEnsembleQFunction.default(environment, jit_compile=False)
-        if policy is None:
-            policy = NNPolicy.default(environment, jit_compile=False)
+    def custom_opt_step(self):
+        self.algorithm.reset_info()
 
-        optimizer = Adam(chain(policy.parameters(), critic.parameters()), lr=lr)
+        device = self.device if self.device == "cpu" else None
+        observation, idx, weight = self.memory.sample_batch(
+            self.batch_size, device=device
+        )
+        with DisableGradient(self.algorithm.policy):
+            self.critic_optimizer.zero_grad()
+            critic_loss = self.algorithm.critic_loss(observation)
+            loss = (critic_loss.critic_loss * weight.detach()).mean()
+            loss.backward()
+            self.critic_optimizer.step()
+
+        with DisableGradient(self.algorithm.critic):
+            self.actor_optimizer.zero_grad()
+            actor_loss = self.algorithm.actor_loss(observation)
+            loss = (actor_loss.policy_loss * weight.detach()).mean()
+            loss.backward()
+            self.actor_optimizer.step()
+
+        if not isinstance(self.algorithm.eta, Constant):
+            self.temp_optimizer.zero_grad()
+            temp_loss = self.algorithm.regularization_loss(observation)
+            loss = (temp_loss.dual_loss * weight.detach()).mean()
+            loss.backward()
+        else:
+            tmep_loss = Loss()
+
+        self.temp_optimizer.step()
+
+        # Update memory
+        self.memory.update(idx, critic_loss.td_error.abs().detach())
+
+        return actor_loss + critic_loss + temp_loss
+
+    @classmethod
+    def default(
+        cls,
+        environment,
+        policy=None,
+        critic=None,
+        lr=3e-4,
+        fixed_temperature=False,
+        layers=None,
+        non_linearity="ReLU",
+        eta=1.0,
+        *args,
+        **kwargs,
+    ):
+        """See `AbstractAgent.default'."""
+        layers = layers or [256, 256]
+        if critic is None:
+            critic = NNEnsembleQFunction.default(
+                environment,
+                layers=layers,
+                non_linearity=non_linearity,
+                jit_compile=False,
+            )
+        if policy is None:
+            policy = NNPolicy.default(
+                environment,
+                layers=layers,
+                non_linearity=non_linearity,
+                log_scale=True,
+                min_scale=-20.0,
+                max_scale=1.5,
+                jit_compile=False,
+            )
+
+        if fixed_temperature:
+            eta = Constant(eta)
+        else:
+            eta = Learnable(eta, positive=True)
+
+        policy_optimizer = Adam(policy.parameters(), lr=lr)
+        critic_optimizer = Adam(critic.parameters(), lr=lr)
+        temp_optimizer = Adam(eta.parameters(), lr=lr)
 
         return super().default(
             environment,
             critic=critic,
             policy=policy,
-            optimizer=optimizer,
+            eta=eta,
+            actor_optimizer=policy_optimizer,
+            critic_optimizer=critic_optimizer,
+            temp_optimizer=temp_optimizer,
             *args,
             **kwargs,
         )
--- rllib/algorithms/sac.py
+++ rllib/algorithms/sac.py
@@ -1,5 +1,10 @@
 """Soft Actor-Critic Algorithm."""
+import torch
 
+from rllib.dataset.datatypes import Loss
+from rllib.util.neural_networks import DisableGradient
+from rllib.util.utilities import tensor_to_distribution
+from rllib.value_function.nn_ensemble_value_function import NNEnsembleQFunction
 from .abstract_algorithm import AbstractAlgorithm
 
 
@@ -25,6 +30,7 @@ class SAC(AbstractAlgorithm):
         super().__init__(
             eta=eta, entropy_regularization=entropy_regularization, *args, **kwargs
         )
+        self.eta = eta
         assert (
             len(self.policy.dim_action) == 1
         ), "Only Nx1 continuous actions implemented."
@@ -37,4 +43,82 @@ class SAC(AbstractAlgorithm):
 
     def actor_loss(self, observation):
         """Get Actor Loss."""
-        return self.pathwise_loss(observation).reduce(self.criterion.reduction)
+        assert isinstance(self.critic, NNEnsembleQFunction)
+        state = observation.state
+        pi = tensor_to_distribution(self.policy(state), **self.policy.dist_params)
+        action_normalized = pi.rsample()
+        log_p = pi.log_prob(action_normalized)
+
+        action = self.policy.action_scale * action_normalized
+        with DisableGradient(self.critic):
+            q = self.critic(state, action)
+            q = torch.min(q, -1).values
+        # Take mean over time coordinate.
+        if 1 < q.dim():
+            q = q.mean(dim=1)
+            log_p = log_p.mean(dim=1)
+        actor_loss = -q + self.entropy_loss.eta * log_p
+
+        return Loss(policy_loss=actor_loss)
+
+    def critic_loss(self, observation):
+        """Get critic loss.
+
+        This is usually computed using fitted value iteration and semi-gradients.
+        critic_loss = criterion(pred_q, target_q.detach()).
+
+        Parameters
+        ----------
+        observation: Observation.
+            Sampled observations.
+            It is of shape B x N x d, where:
+                - B is the batch size
+                - N is the N-step return
+                - d is the dimension of the attribute.
+
+        Returns
+        -------
+        loss: Loss.
+            Loss with parameters loss, critic_loss, and td_error filled.
+        """
+        pred_q = self.get_value_prediction(observation)
+
+        # Get target_q with semi-gradients.
+        with torch.no_grad():
+            target_q = self.get_value_target(observation)
+
+            target_q = target_q.unsqueeze(-1).repeat_interleave(
+                self.critic.num_heads, -1
+            )
+
+        critic_loss = self.criterion(pred_q, target_q)
+
+        # Ensembles have last dimension as ensemble head; sum all ensembles.
+        critic_loss = critic_loss.sum(-1)
+        # Take mean over time coordinate.
+        critic_loss = critic_loss.mean(-1)
+
+        return Loss(critic_loss=critic_loss)
+
+    def regularization_loss(self, observation, num_trajectories=1):
+        state = observation.state
+        pi = tensor_to_distribution(self.policy(state), **self.policy.dist_params)
+        action_normalized = pi.rsample()
+        log_p = pi.log_prob(action_normalized)
+
+        # Take mean over time coordinate.
+        if 1 < log_p.dim():
+            log_p = log_p.mean(dim=1)
+
+        # Compute dual loss at the same time
+        dual_loss = (
+            self.entropy_loss._eta()
+            * (-log_p - self.entropy_loss.target_entropy).detach()
+        )
+
+        entropy = -pi.log_prob(pi.sample([4])).mean().detach()
+        self._info.update(
+            eta=self.entropy_loss.eta,
+            entropy=self._info["entropy"] + entropy,
+        )
+        return Loss(dual_loss=dual_loss)
